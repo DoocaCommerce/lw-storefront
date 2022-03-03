d3da042be10f45041c9250cdf6564140705172c0
Antony dos Reis <lucasantonydosreis@gmail.com>
2022-03-03 11:19:22 -0300

    fix: correções no componente

--- src/react/components/meta-seo/index.tsx
+++ src/react/components/meta-seo/index.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { Helmet } from 'react-helmet'
 import DefaultTags from './partials/default-tags'
 import FacebookTags from './partials/facebook-tags'
 import TwitterTags from './partials/twitter-tags'
@@ -7,10 +6,10 @@ import { SeoProps } from './types'
 
 export function MetaSeo({ data }: SeoProps) {
   return (
-    <Helmet defer={false}>
+    <>
       <DefaultTags data={data} />
       <FacebookTags data={data} />
       <TwitterTags data={data} />
-    </Helmet>
+    </>
   )
 }
