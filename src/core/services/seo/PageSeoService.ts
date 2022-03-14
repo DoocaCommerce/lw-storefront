91a0932de2a38310dd174c022bb5f7cadf3ca0a6
Antony dos Reis <lucasantonydosreis@gmail.com>
2022-03-14 08:18:44 -0300

    fix: alterado de data para page

--- src/core/services/seo/PageSeoService.ts
+++ src/core/services/seo/PageSeoService.ts
@@ -3,42 +3,40 @@ import { SeoService } from './SeoService'
 import { PageMicroData } from './types'
 
 export class PageSeoService extends SeoService {
-  data: Page
+  page: Page
 
   constructor(data: Page) {
     super()
-    this.data = data
+    this.page = data
   }
 
   public getPage(): string {
-    if (this.data.template === 'faq') return this.render(this.faqPage())
+    if (this.page.template === 'faq') return this.render(this.faqPage())
     return this.render(this.genericPage())
   }
 
   private genericPage(): PageMicroData {
-    let page = this.data
     let microData: PageMicroData = {
       '@context': 'http://schema.org/',
       '@type': 'WebPage',
-      url: page.url,
-      name: page.name,
-      description: this.formatRichText(page.description)
+      url: this.page.url,
+      name: this.page.name,
+      description: this.formatRichText(this.page.description)
     }
     return microData
   }
 
   private faqPage(): PageMicroData {
-    let page = this.data
     let microData: PageMicroData = {
       '@context': 'http://schema.org/',
       '@type': 'FAQPage',
-      url: page.url,
-      name: page.name,
-      description: this.formatRichText(page.description),
+      url: this.page.url,
+      name: this.page.name,
+      description: this.formatRichText(this.page.description),
       mainEntity: []
     }
-    if (page.faq.length) {
-      page.faq.forEach(question => {
+    if (this.page.faq.length) {
+      this.page.faq.forEach(question => {
         let questionItem = {
           '@type': 'Question',
           name: question.name,
