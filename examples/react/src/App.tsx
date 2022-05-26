9380818220a3f52e42d9e6871cb346f66e9795dd
tiagotermedooca <89025816+tiagotermedooca@users.noreply.github.com>
2022-05-26 09:15:02 -0300

    fix:config package

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,34 +1,12 @@
 import { useEffect } from 'react'
-import { services } from 'lw-storefront/lib/core'
-
-const { brand, sections, settings } = services
-
-const get = async (id: string) => {
-  const result = await settings.getSettings()
-  console.log(`result ${id}`, result)
-}
-
-function Header() {
-  useEffect(() => {
-    const get = async (id: string) => {
-      const result = await brand.getBrandList({page: 1, first: 3})
-      console.log(`result ${id}`, result)
-    }
-    get('header')
-  }, [])
-  return <h1>Hello </h1>
-}
+import { useSettings } from 'lw-storefront/lib/react/hooks/use-settings'
 
 function App() {
+  const setting = useSettings()
   useEffect(() => {
-    get('app')
-  }, [])
-
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  )
+    console.log(setting)
+  }, [setting])
+  return <div className="App">Teste </div>
 }
 
 export default App
