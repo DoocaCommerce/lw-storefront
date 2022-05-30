6d259420d314e9e8f187ff3112f0054d14e17dc2
tiagotermedooca <89025816+tiagotermedooca@users.noreply.github.com>
2022-05-30 15:33:11 -0300

    fix: fix service

--- src/core/modules/pages/PagesService.ts
+++ src/core/modules/pages/PagesService.ts
@@ -1,20 +1,19 @@
-import { PagesRepository } from "./PagesRepository"
-import { Page, PageFields } from "./PageTypes"
-import "isomorphic-fetch"
+import { PagesRepository } from './PagesRepository'
+import { Page, PageFields } from './PageTypes'
 
 export class PagesService {
-    static async getPageList(fields?: Array<PageFields>): Promise<Array<Page>> {
-      const result:Array<Page> = await PagesRepository.getPageList(fields)
-      return result
-    }
+  static async getPageList(fields?: Array<PageFields>): Promise<Array<Page>> {
+    const result: Array<Page> = await PagesRepository.getPageList(fields)
+    return result
+  }
 
-    static async getPageById(id: Number, fields?: Array<PageFields>): Promise<Page> {
-      const result:Page = await PagesRepository.getPageById(id, fields)
-      return result
-    } 
-  
-    static async getPageBySlug(slug: String, fields?: Array<PageFields>): Promise<Page> {
-      const result:Page = await PagesRepository.getPageBySlug(slug, fields)
-      return result
-    } 
+  static async getPageById(id: Number, fields?: Array<PageFields>): Promise<Page> {
+    const result: Page = await PagesRepository.getPageById(id, fields)
+    return result
+  }
+
+  static async getPageBySlug(slug: String, fields?: Array<PageFields>): Promise<Page> {
+    const result: Page = await PagesRepository.getPageBySlug(slug, fields)
+    return result
+  }
 }
