5b2b9d9a173e79be22bc25da30e16347617990ae
diovaniBelleboni <diovani@dooca.com.br>
2022-06-08 09:47:00 -0300

    feat: adicionado fluxo de busca de lista de menu

--- src/core/modules/menu/MenuService.ts
+++ src/core/modules/menu/MenuService.ts
@@ -6,4 +6,9 @@ export class MenuService {
     const result: Menu = await MenuRepository.getMenuById(id, fields)
     return result
   }
+
+  static async getMenuList(fields?: Array<MenuFields>): Promise<Array<Menu>> {
+    const result: Array<Menu> = await MenuRepository.getMenuList(fields)
+    return result
+  }
 }
