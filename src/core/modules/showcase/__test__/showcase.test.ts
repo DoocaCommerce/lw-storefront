d9f36af8322f55fff3c8c0b5b68fe851ee39a8ab
diovaniBelleboni <diovani@dooca.com.br>
2022-06-30 11:27:27 -0300

    test: adicionado teste de busca d elista de showcases

--- src/core/modules/showcase/__test__/showcase.test.ts
+++ src/core/modules/showcase/__test__/showcase.test.ts
@@ -5,14 +5,14 @@ import 'isomorphic-fetch'
 const SELECTED_FIELDS: Array<ShowcaseFields> = ['id', 'slug']
 
 describe('Showcase Module', () => {
-  it('Should get showcase and by id with all fields successfully', async () => {
+  it('Should get showcase by id with all fields successfully', async () => {
     const FILTER_ID = 64428
     const showcaseResult = await ShowcaseService.getShowcaseById(FILTER_ID)
     expect(showcaseResult.id).toEqual(FILTER_ID.toString())
     expect(showcaseResult.name).toEqual('Produtodeteste')
   })
 
-  it('Should get showcase and by id with all fields successfully', async () => {
+  it('Should get showcase by id with selected fields successfully', async () => {
     const FILTER_ID = 64428
     const showcaseResult = await ShowcaseService.getShowcaseById(FILTER_ID, [...SELECTED_FIELDS])
     const showcaseResultFields = Object.keys(showcaseResult).filter(key => key != '__typename')
@@ -20,7 +20,7 @@ describe('Showcase Module', () => {
     expect(showcaseResultFields.length).toEqual(SELECTED_FIELDS.length)
   })
 
-  it('Should get showcase and by slug with all fields successfully', async () => {
+  it('Should get showcase by slug with all fields successfully', async () => {
     const FILTER_SLUG = 'sdfsd'
     const showcaseResult = await ShowcaseService.getShowcaseBySlug(FILTER_SLUG)
     expect(showcaseResult.slug).toEqual(FILTER_SLUG)
@@ -33,5 +33,10 @@ describe('Showcase Module', () => {
     expect(showcaseResult.slug).toEqual('sdfsd')
     expect(showcaseResult.name).toEqual('Produtodeteste')
     expect(showcaseResult.id).toEqual('64428')
+  })
+
+  it('Should get showcase list with all fields successfully', async () => {
+    const showcaseResult = await ShowcaseService.getShowcaseList({ page: 1, first: 1 })
+    expect(showcaseResult.edges.length).toEqual(1)
   })
 })
