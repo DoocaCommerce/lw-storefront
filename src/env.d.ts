63068e1d5e7dce7b49e4e39e385aea488ddc8d76
diovaniBelleboni <diovani@dooca.com.br>
2022-07-04 09:55:13 -0300

    refactor: refatorada repository para novo padrao

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,11 @@
 interface dc_config {
-  token: string,
-  api_url: string,
-  domain: string,
+  token: string
+  api_url: string
+  domain: string
   mock?: {
     sections?: any
     settings?: any
+    scripts?: any
   }
 }
 
