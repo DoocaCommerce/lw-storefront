eb692e464d6cefcab72c16ae2a1c4b7098f2d767
diovaniBelleboni <diovani@dooca.com.br>
2022-07-05 06:53:23 -0300

    fix: refatorada service de brands, nomenclaturas e testes

--- src/core/modules/brand/BrandService.ts
+++ src/core/modules/brand/BrandService.ts
@@ -1,29 +1,32 @@
 import { PaginationFilter } from '../../types/PaginationTypes'
-import { BrandRepository } from './BrandRepository'
+import { BrandRepositoryGql } from './BrandRepositoryGql'
+import { BrandRepositoryJson } from './BrandRepositoryJson'
 import { Brand, BrandFields, BrandList } from './BrandTypes'
 
+const Repository = dc_config.mock?.brand ? BrandRepositoryJson : BrandRepositoryGql
+
 export class BrandService {
-  static async getBrandList(pagenationFilter: PaginationFilter, fields?: Array<BrandFields>): Promise<BrandList> {
+  static async getList(pagenationFilter: PaginationFilter, fields?: Array<BrandFields>): Promise<BrandList> {
     try {
-      const result: BrandList = await BrandRepository.getBrandList({ fields: fields || null, filter: pagenationFilter })
+      const result: BrandList = await Repository.getList({ fields: fields || null, filter: pagenationFilter })
       return result
     } catch (error) {
       throw new Error(error)
     }
   }
 
-  static async getBrandById(id: Number, fields?: Array<BrandFields>): Promise<Brand> {
+  static async getById(id: Number, fields?: Array<BrandFields>): Promise<Brand> {
     try {
-      const result: Brand = await BrandRepository.getBrandById(id, fields)
+      const result: Brand = await Repository.getById(id, fields)
       return result
     } catch (error) {
       throw new Error(error)
     }
   }
 
-  static async getBrandBySlug(slug: String, fields?: Array<BrandFields>): Promise<Brand> {
+  static async getBySlug(slug: String, fields?: Array<BrandFields>): Promise<Brand> {
     try {
-      const result: Brand = await BrandRepository.getBrandBySlug(slug, fields)
+      const result: Brand = await Repository.getBySlug(slug, fields)
       return result
     } catch (error) {
       throw new Error(error)
