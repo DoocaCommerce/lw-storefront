028a238652cbd34babcdd2b12368a9c15beb5531
diovaniBelleboni <diovani@dooca.com.br>
2022-07-12 13:52:36 -0300

    feat: adicionado hook de landing pages

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,6 +1,7 @@
 export { useVariable } from './hooks/use-variables'
 export { useSettings } from './hooks/use-settings'
 export { useSections } from './hooks/use-sections'
+export { useLandingPages } from './hooks/use-landing-pages'
 export { SectionLoader } from './components/section-loader'
 export { Sections } from './components/sections'
 export { StoreProvider } from './components/store-provider'
